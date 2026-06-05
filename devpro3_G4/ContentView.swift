1950e8270d1a6e589caf748d6274f1030a4dd623
kouki-stk <kouki060108@gmail.com>
2026-06-05 16:54:34 +0900

    Initial Commit

---
+++ devpro3_G4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  devpro3_G4
+//
+//  Created by 齊藤 洸希 on 2026/06/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
